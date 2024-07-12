92b01cd22743e2f7a9a4d82c6312a3ca5a8f3d12
pyon123 <pyon123@outlook.com>
2024-07-12 11:01:06 -0400

    add Resource field validation

--- dto/execute.py
+++ dto/execute.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, field_validator
 from enum import Enum
 
 class TaskType(Enum):
@@ -9,6 +9,18 @@ class ResourceType(BaseModel):
     gpu: str
     ram: str
     storage: str
+
+    @field_validator('cpu', 'gpu')
+    def validate_positive_int(cls, v: str) -> int:
+        if not v.isdigit():
+            raise ValueError(f'{v} should be a valid non-negative integer string.')
+        return int(v)
+
+    @field_validator('ram', 'storage')
+    def validate_memory_format(cls, v: str) -> str:
+        if not v[:-2].isdigit() or v[-2:].upper() not in ['MB', 'GB']:
+            raise ValueError(f'{v} is not a valid format.')
+        return v
 
 class RequestDTO(BaseModel):
     task_type: TaskType
